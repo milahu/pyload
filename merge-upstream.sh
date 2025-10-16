9e2226708f33cedda2cfa219ccb2936d1e9035d3
Milan Hauth <milahu@gmail.com>
2025-10-16 13:28:43 +0200

    git-stash before git-merge

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -9,5 +9,9 @@ git fetch https://github.com/pyload/pyload develop:upstream-develop
 # rebase: change commit ids
 # git rebase upstream-develop
 
+git stash -m "merge-upstream.sh $(date -Is)"
+
 # merge: preserve commit ids
 git merge upstream-develop
+
+git stash pop
